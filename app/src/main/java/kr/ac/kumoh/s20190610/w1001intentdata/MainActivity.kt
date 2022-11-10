3f67a00a8e4d530117059eba04775d9a5ae9f76f
SonDaAe <103577644+SonDaAe@users.noreply.github.com>
2022-11-10 12:55:33 +0900

    스마트앱프로그래밍 10주차 01 좀 더 좋은 코드

--- app/src/main/java/kr/ac/kumoh/s20190610/w1001intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190610/w1001intentdata/MainActivity.kt
@@ -3,26 +3,61 @@ package kr.ac.kumoh.s20190610.w1001intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.startActivity
+import kr.ac.kumoh.s20190610.w1001intentdata.MainActivity.Companion.keyName
 import kr.ac.kumoh.s20190610.w1001intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        const val keyName = "image"
+    }
     private lateinit var binding: ActivityMainBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGundam.setOnClickListener {
-            val intent = Intent(this, ImageActivity::class.java)
-            //intent.putExtra("image", R.drawable.gundam)
-            intent.putExtra("image", "gundam")
-            startActivity(intent)
-        }
+        binding.btnGundam.setOnClickListener(this)
+        binding.btnZaku.setOnClickListener(this)
 
-        binding.btnZaku.setOnClickListener {
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra("image", "zaku")
-            startActivity(intent)
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+
+            if (it.resultCode != RESULT_OK)
+                return@registerForActivityResult
+
+            val result = it.data?.getIntExtra(
+                ImageActivity.resultName,
+                ImageActivity.NONE)
+            val str = when (result) {
+                ImageActivity.LIKE -> "좋아요"
+                ImageActivity.DISLIKE -> "싫어요"
+                else -> ""
+            }
+
+            val image = it.data?.getStringExtra(ImageActivity.imageName)
+            when (image) {
+                "gundam" -> binding.btnGundam.text = "건담 ($str)"
+                "zaku" -> binding.btnZaku.text = "자쿠 ($str)"
+            }
         }
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this,
+            ImageActivity::class.java)
+        val value = when (v?.id) {
+            binding.btnGundam.id -> "gundam"
+            binding.btnZaku.id -> "zaku"
+            else -> return
+        }
+        intent.putExtra(keyName, value)
+        //startActivity(intent)
+        launcher.launch(intent)
     }
 }
